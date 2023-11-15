bbc093f8ea8c151c5b3ae0710f16028ba6dce304
Letícia Costa <leeleticiac@gmail.com>
2023-11-14 21:53:23 -0300

    atualizacoes page algoritmos

--- my-app/src/components/AlgoritmoBox/index.tsx
+++ my-app/src/components/AlgoritmoBox/index.tsx
@@ -17,81 +17,85 @@ const Algoritmo = ({ conditions, setConditions }: AlgoritmoProps) => {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
 
+  const atualizarPaginacao = (value: string) => {
+    if (value === 'FIFO') {
+      setConditions({ ...conditions, paginacao: 'MRU' });
+    }
+    if (value === 'MRU') {
+      setConditions({ ...conditions, paginacao: 'FIFO' });
+    }
+  };
+
   return (
     <div className="corpo">
-      <section>
-        <form className="algoritmo" onSubmit={(e) => e.preventDefault()}>
+      <form className="algoritmo" onSubmit={(e) => e.preventDefault()}>
+        <section className="metodos">
           <h2 className="titulo">Selecione o Algoritmo:</h2>
-          <div>
-            {opcoesMetodo.map((metodo) => (
-              <li className="botoes">
+          <section className='container-metodos'>
+            <div>
+              {opcoesMetodo.map((metodo) => (
+                <li className="botoes">
+                  <button
+                    className="botao"
+                    type="button"
+                    onClick={() => {
+                      console.log({ conditions });
+                      setConditions({ ...conditions, metodo });
+                    }}
+                  >
+                    {metodo}
+                  </button>
+                </li>
+              ))}
+            </div>
+            <div className="quantum-sobrecarga">
+              <div>
+                <label className="titulo">Quantum:</label>
+                <input
+                  className="botao"
+                  type="number"
+                  id="quantum"
+                  min="1"
+                  value={conditions.quantum}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label className="titulo">Sobrecarga: </label>
+                <input
+                  className="botao"
+                  type="number"
+                  id="sobrecarga"
+                  min="0"
+                  value={conditions.sobrecarga}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </section>
+        </section>
+        <section className="paginacao">
+          <h2 className="titulo">Paginação: </h2>
+          <div className="botoes">
+            {opcoesPaginacao.map((paginacao) => (
+              <li className="botoes" key={paginacao}>
                 <button
                   className="botao"
                   type="button"
+                  id="paginacao"
+                  value={conditions.paginacao}
                   onClick={() => {
+                    atualizarPaginacao(paginacao);
                     console.log({ conditions });
-                    setConditions({ ...conditions, metodo });
                   }}
                 >
-                  {metodo}
+                  {paginacao}
                 </button>
               </li>
             ))}
           </div>
-        </form>
-        <div className="quantum-sobrecarga">
-          <div>
-            <label className="titulo">Quantum:</label>
-            <input
-              className="botao"
-              type="number"
-              id="quantum"
-              min="1"
-              value={conditions.quantum}
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <label className="titulo">Sobrecarga: </label>
-            <input
-              className="botao"
-              type="number"
-              id="sobrecarga"
-              min="0"
-              value={conditions.sobrecarga}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-      </section>
-      <section className="paginacao">
-        <h2 className="titulo">Paginação: </h2>
-        <div className="botoes">
-          <input
-            className="botao"
-            id="intervalo"
-            type="number"
-            value={conditions.intervalo}
-            onChange={handleChange}
-          />
-          {opcoesPaginacao.map((paginacao) => (
-            <li className="botoes">
-              <button
-                className="botao"
-                type="button"
-                id="paginacao"
-                value={paginacao}
-                onClick={() => {
-                  console.log({ conditions });
-                  setConditions({ ...conditions, paginacao });
-                }}
-              >
-                {paginacao}
-              </button>
-            </li>
-          ))}
-        </div>
-      </section>
+        </section>
+      </form>
     </div>
   );
 };
