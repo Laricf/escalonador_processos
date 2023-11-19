4b4fcb2065f606e62a839d29db9c3df93750ca42
Letícia Costa <leeleticiac@gmail.com>
2023-11-19 16:59:11 -0300

    att processoCard

--- my-app/src/components/ProcessoCard/index.tsx
+++ my-app/src/components/ProcessoCard/index.tsx
@@ -4,23 +4,23 @@ function ProcessoCard() {
   return (
     <div className="card">
         <div className='boxCard'>
-          <label htmlFor="" className="label">
+          <label htmlFor="tempo" className="label">
             Tempo:
-            <input className="btn" type="number" min='1' />
+            <input className="btn" id='tempo' type="number" min='1' />
           </label>
-          <label htmlFor="" className="label">
+          <label htmlFor="deadline" className="label">
             Deadline:
-            <input className="btn" type="number" min='0' />
+            <input id='deadline' className="btn" type="number" min='0' />
           </label>
         </div>
         <div className='boxCard'>
-        <label htmlFor="" className="label">
+        <label htmlFor="chegada" className="label">
         Chegada:
-            <input className="btn" type="number" min='1' />
+            <input className="btn" type="number" id='chegada' min='1' />
           </label>
-          <label htmlFor="" className="label">
+          <label htmlFor="paginas" className="label">
             Páginas:
-            <input className="btn" type="number" />
+            <input className="btn" type="number" id='paginas' min='0' />
           </label>
         </div>
         <button className='exit'></button>
